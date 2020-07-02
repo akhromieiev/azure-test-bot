0ad51c5dca6941c5688d551e09979e6ef38a88cc
ARoman <qpbd@ukr.net>
2020-07-02 21:13:36 +0300

    Added webhook

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(ctx => ctx.reply('Hello'));
 bot.on('message', ctx => ctx.telegram.sendCopy(ctx.chat.id, ctx.message));
-bot.launch();
+bot.startWebhook('/secret-path', null, 8080);
